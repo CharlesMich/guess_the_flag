069082a47a9e8674dd1da34325b37fa92ccf48d1
CharlesMich <charleedongo@hotmail.com>
2024-12-31 23:11:44 -0600

    Initial Commit

---
+++ guessTheFlag2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  guessTheFlag2
+//
+//  Created by Charles Michael on 12/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ guessTheFlag2/guessTheFlag2App.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlag2App.swift
+//  guessTheFlag2
+//
+//  Created by Charles Michael on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlag2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
